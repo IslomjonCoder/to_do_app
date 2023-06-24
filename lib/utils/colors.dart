44c45d0c453a368d6699c41990c813f8cc48491b
islom <islomjon20010930@gmail.com>
2023-06-24 20:26:36 +0500

    Fonts added

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorsApp {
+  static const Color c_FFFFFF = Color(0xFFFFFFFF);
+  static const Color c_090A0A = Color(0xFF090A0A);
+  static const Color c_4683F9 = Color(0xFF4683F9);
+  static const Color c_B53CC9 = Color(0xFFB53CC9);
+  static const Color c_FED608 = Color(0xFFFED608);
+  static const Color c_0FD25D = Color(0xFF0FD25D);
+}
